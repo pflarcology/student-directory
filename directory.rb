250cd8ddda626675c37eac4d7cc4aae27f3d6e75
Rory Grieve <rhrg500@gmail.com>
2016-10-24 13:03:05 +0000

    start with letter

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-# let's put all the students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_studnets
+  puts "Please enter the names of students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is empty, repeat this code
+  while !name.empty? do
+      # add the student hash to the array
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -19,15 +23,26 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    if student[:name].start_with?('d', 'D')
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    else
+    end
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  count = 0 
+  names.each do |student|
+    if student[:name].start_with?('d', 'D')
+      count += 1
+    else
+    end
+  end
+  puts "Overall, we have #{count} students"
 end
-# nothing happens until we call the methods
+
+students = input_studnets
 print_header
 print(students)
 print_footer(students)
