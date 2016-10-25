b2c20a174d095f94d8dd8dd744669ed639cfad32
Rory Grieve <rhrg500@gmail.com>
2016-10-25 15:20:15 +0000

    refactored code

--- directory.rb
+++ directory.rb
@@ -1,8 +1,44 @@
+
+@students = [] # an empty array accessible to all methods
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items later
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      @students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know you meant, try again"
+  end
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
+  @students = []
   # get the first name
   name = gets.strip
   # while the name is not empty, repeat this code
@@ -18,17 +54,17 @@ def input_students
     puts "Did you type that correctly?"
       cohort = gets.strip
     end
-    students << {name: name, cohort: cohort.to_sym}
-    if students.count == 1
+    @students << {name: name, cohort: cohort.to_sym}
+    if @students.count == 1
       puts "Now we have 1 student"
     else 
-      puts "No we have #{students.count} students"
+      puts "No we have #{@students.count} students"
     end
     # get another name from the user
     name = gets.strip
   end
   # return the array of students
-  students
+  @students
 end
 
 def print_header
@@ -36,7 +72,7 @@ def print_header
     puts ("-------------".center(50))
 end
 
-def print(students)
+def print_students_list(students)
   if students.count > 0
     sorted = students.sort_by { |hsh| hsh[:cohort] }
     sorted.each do |sorted|
@@ -49,31 +85,6 @@ end
 
 def print_footer(students)
     puts("Overall, we have #{students.count} great students".center(50))
-end
-
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items later
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know you meant, try again"
-    end
-  end
 end
 
 interactive_menu
