830a4e31d8ffe8bf0e95037beeb78695982438eb
Rory Grieve <rhrg500@gmail.com>
2016-10-25 11:17:47 +0000

    centered

--- directory.rb
+++ directory.rb
@@ -1,50 +1,46 @@
-def input_studnets
-  puts "Please enter the names of students"
+def input_students
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
-      # add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-      # get another name from the user
-      name = gets.chomp
+    # add the student hash to the array
+    puts "What about their country of birth?"
+    cob = gets.chomp
+    puts "And most importantly, what's their height?"
+    height = gets.chomp
+    students << {name: name, cohort: :november, height: height, cob: cob}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
   # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts ("The students of Villains Academy".center(50))
+  puts ("-------------".center(50))
 end
 
 def print(students)
   count = 0
-  students.each_with_index do |student, index|
-    if student[:name].length <= 12
-      count += 1
-      puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)"
-    else
-    end
+  total = students.size
+  while count < total
+    puts ("#{count+1} #{students[count][:name]} #{students[count][:cob]} #{students[count][:height]}".center(50))
+    count += 1
   end
 end
 
-def print_footer(names)
-  count = 0 
-  names.each do |student|
-    if student[:name].length <= 12
-      count += 1
-    else
-    end
-  end
-  puts "Overall, we have #{count} students"
+def print_footer(students)
+  puts("Overall, we have #{students.count} great students".center(50))
 end
 
-students = input_studnets
+students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
