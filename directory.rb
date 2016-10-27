6b6de0fa035f3b5141c918e89c85841848276ebf
Rory Grieve <rhrg500@gmail.com>
2016-10-27 12:39:56 +0000

    adding students method

--- directory.rb
+++ directory.rb
@@ -7,22 +7,20 @@ def save_students
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(',')
+    csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
 end
 
-
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+  name, cohort = line.chomp.split(',')
+    adding_students(name, cohort)
   end
   file.close
 end
-
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
@@ -46,14 +44,14 @@ end
 
 def show_students
   print_header
-  print_students_list(@students)
-  print_footer(@students)
+  print_students_list
+  print_footer
 end
 
 def process(selection)
   case selection
     when "1"
-      @students = input_students
+      input_students
     when "2"
       show_students
     when "3"
@@ -75,45 +73,45 @@ def interactive_menu
   end
 end
 
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  @students = []
   # get the first name
   name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    if @students.count == 1
-      puts "Now we have 1 student"
-    else 
-      puts "No we have #{@students.count} students"
-    end
+    puts "What cohort are they part of?"
+    cohort = STDIN.gets.chomp
+    adding_students(name, cohort)
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
   end
 end
+
+
+def adding_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
 
 def print_header
     puts ("The students of Villains Academy".center(50))
     puts ("-------------".center(50))
 end
 
-def print_students_list(students)
-  if students.count > 0
-    sorted = students.sort_by { |hsh| hsh[:cohort] }
-    sorted.each do |sorted|
-      puts "#{sorted[:name]} #{sorted[:cohort]}"
-    end
-  else
-    puts ("There are no students at Villains Academy".center(50))
+
+def print_students_list
+  @students.each do |student|
+   puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-    puts("Overall, we have #{@students.count} great students".center(50))
+
+def print_footer
+    puts("Overall, we have  great students".center(50))
 end
 
 try_load_students
